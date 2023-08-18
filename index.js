43e7e978e4652272ca033906ff5982ae32d82ef8
Mann <Mann@88.com>
2023-08-18 15:27:57 +0530

    stocklist fetch

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const port = 5000;
 
 app.use(express.json());
 
-const stockSaveToDB = require('./stockdb');
-
+const { stockSaveToDB, updateStocks } = require('./stockdb');
 // dedicated to web3 world
 app.get('/gm', (req, res) => {
     res.send('GM!');
@@ -15,13 +14,34 @@ app.get('/gm', (req, res) => {
 
 app.post('/api/addstocks', async (req, res) => {
 const { stocks , userEmail } = req.body;
-    
+
     try {
         const result = await stockSaveToDB(stocks, userEmail );
 
         if (result.status) {
             res.status(200).json({ message: 'Function executed and data saved', user: result.user });
         } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.post('/api/updatestocks',async(req,res)=>{
+    const {email}=req.body;
+    console.log(req.body);
+    console.log("Email from parent:", email);
+    
+    try{ 
+        const listedStocks = await updateStocks(email);
+
+        if (listedStocks) {
+            console.log("Stocks in parent", listedStocks);
+            res.status(200).json({ status: true, message: 'Stocks listed successfully', stocks: listedStocks[0].selectedStocks });
+        } 
+        else {
             res.status(404).json({ message: 'User not found' });
         }
     } catch (error) {
--- stockdb.js
+++ stockdb.js
@@ -23,6 +23,49 @@ const stocklistSchema = new mongoose.Schema({
 
 const Stocks = mongoose.model('stocklist', stocklistSchema);
 
+async function fetchStocklist(userEmail) {
+    const client = new MongoClient(MONGODB_URI);
+    const dbName = 'test';       // Replace with your database name
+    const collectionName = 'stocklists'; // Replace with your collection name
+
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        console.log(5);
+        const documents = await collection.find({ userEmail }).toArray();
+        console.log(6);
+        console.log('Fetched documents:', documents);
+        return documents;
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        client.close();
+    }
+}
+async function updateStocks(email) {
+    console.log("updateStocks calling");
+    console.log(1);
+    try {
+        const documents = await fetchStocklist(email);
+        const listedStocks = [];
+        console.log("Documents from main function", documents);
+        for (let i = 0; i < documents.length; i++) {
+            if (documents[i].userEmail === email) {
+                listedStocks.push(documents[i]);
+            }
+        }
+        console.log("Stocks:", listedStocks);
+        return listedStocks;
+    }
+    catch (error) {
+        console.error('Error:', error);
+    } finally {
+        client.close();
+    }
+}
 
 async function fetchEmail(userEmail) {
     const dbName = 'test';
@@ -39,58 +82,55 @@ async function fetchEmail(userEmail) {
     return documents;
 }
 
-
 const { MongoClient } = require('mongodb');
 
 async function updateField(documentId, updatedValue) {
     const url = MONGODB_URI; // Replace with your MongoDB server URL
-const dbName = 'test';       // Replace with your database name
-const collectionName = 'stocklists'; // Replace with your collection name
+    const dbName = 'test';       // Replace with your database name
+    const collectionName = 'stocklists'; // Replace with your collection name
 
-const client = new MongoClient(url);
+    const client = new MongoClient(url);
 
-  try {
-    await client.connect();
-    console.log('Connected successfully to server');
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
 
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
 
-    const filter = { _id: documentId }; // Replace with the actual field to identify the document
-    console.log(filter);
-    const update = { $set: { selectedStocks: updatedValue } }; // Replace fieldName with the actual field name
-    console.log(filter);
+        const filter = { _id: documentId }; // Replace with the actual field to identify the document
+        console.log(filter);
+        const update = { $set: { selectedStocks: updatedValue } }; // Replace fieldName with the actual field name
+        console.log(filter);
 
-    const result = await collection.updateOne(filter, update);
-    console.log(result);
+        const result = await collection.updateOne(filter, update);
+        console.log(result);
 
-    console.log(`${result.modifiedCount} document(s) updated.`);
-    return { status: true };
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    client.close();
-  }
+        console.log(`${result.modifiedCount} document(s) updated.`);
+        return { status: true };
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        client.close();
+    }
 }
 
-
-
-
 async function stockSaveToDB(stocksList, email) {
+    console.log("stockdb function calling");
     const record = await fetchEmail(email)
-    if(record[0] && record[0].userEmail == email){
-       console.log("Update")
-       const response = await updateField(record[0]._id, stocksList)
-       return response;
+    if (record[0] && record[0].userEmail == email) {
+        console.log("Update")
+        const response = await updateField(record[0]._id, stocksList)
+        return response;
     } else {
         try {
             const newStockList = new Stocks({
                 selectedStocks: stocksList,
                 userEmail: email,
             });
-    
+
             const data = await newStockList.save();
-    
+
             return { status: true, user: data };
         } catch (err) {
             console.error('An error occurred:', err);
@@ -99,4 +139,4 @@ async function stockSaveToDB(stocksList, email) {
     }
 }
 
-module.exports = stockSaveToDB;
+module.exports = {stockSaveToDB, updateStocks};
