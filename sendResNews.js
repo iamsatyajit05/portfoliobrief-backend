5f52e3b4c8d9ba80003bff97883c10f34a4c4b9e
Mann <Mann@88.com>
2023-08-25 12:47:41 +0530

    news bug fixed

--- sendResNews.js
+++ sendResNews.js
@@ -14,7 +14,7 @@ async function fetchNews() {
         const documents = await collection.find({}).toArray();
 
         const filteredData = await collection.find({}).toArray();
-        
+       
         return filteredData;
     } catch (error) {
         console.error('Error:', error);
@@ -43,28 +43,30 @@ async function userNews(email) {
     try {
         const documents = await fetchStocklist(email);
         const news = await fetchNews();
-        var listedStocks;
-
-        for (let i = 0; i < documents.length; i++) {
+        
+        let listedStocks = []; 
+    for (let i = 0; i < documents.length; i++) {
             if (documents[i].userEmail == email) {
                 listedStocks = documents[i].selectedStocks;
+               break; 
             }
         }
-
-        const userNews = [];
-
-        for (let i = 0; i <= 5; i++) {
-            for (let j = 0; j <= listedStocks.length; j++) {
-                if (news[i].tag == listedStocks[j]) {
-                    userNews.push(news[i]);
-                }
+        
+      const userNews = news.filter(item => {
+            if (item.tag === "") {
+                return false; 
+            } else {
+               
+                return listedStocks.includes(item.tag);
+                
             }
-        }
-
-        return userNews;
+        });
+     return userNews;
     } catch (error) {
         console.error('Error:', error);
+        throw error; 
     }
 }
+
 
 module.exports = { userNews };
